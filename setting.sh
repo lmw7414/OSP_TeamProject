26115692c7cea8a79246ea8c30ef4fc14981c75a
shyeon0312 <78469670+shyeon0312@users.noreply.github.com>
2021-06-23 15:32:55 +0900

    Add files via upload

---
+++ setting.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+pip3 install requests
+pip3 install flask
+pip3 install bs4
+pip3 install urllib3
+pip3 install argparse
+pip3 install nltk
+pip3 install elasticsearch
+
+pip3 install matplotlib
+pip3 install pandas
+pip3 install numpy
+
+sudo apt-get install fonts-nanum*
+sudo cp /usr/share/fonts/truetype/nanum/Nanum* .local/lib/python3.6/site-packages/matplotlib/mpl-data/fonts/ttf/
+rm -rf /home/ubuntu/.cache/matplotlib/*
+
+
